f9ce6482a13ed996aa9564860a3e20990d585646
manbozo5235235124 <pyradex1@gmail.com>
2026-04-02 20:52:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+require('dotenv').config();
 
+// Discord Bot
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', c => {
+  console.log(`${c.user.tag} Liberty Valley Roleplay Community is online!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+// Express for UptimeRobot
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Liberty Valley Roleplay Bot is alive!');
+});
+
+app.listen(port, () => {
+  console.log('HTTP server on port ' + port + ' for UptimeRobot');
+});
+
+// Error handling to stay online
+process.on('uncaughtException', err => console.error('Uncaught:', err));
+process.on('unhandledRejection', reason => console.error('Unhandled:', reason));
